0967ec4f62843f23a43d73fb40d6b70928e119a8
Grossen07 <gr.ossen@yahoo.com>
2016-03-19 09:10:08 +0200

    Added files via upload

---
+++ FileCom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+namespace SmartShoppingDat
+{
+    public class FileCom
+    {
+        public static void ExportBatteryInfo( Client entity)
+        {
+            string Path = "@E:\\Test";
+            var sb = new StringBuilder();
+            sb.AppendLine(@"ClientsId, Username, First Name, Last Name, Adress, Phone");
+            sb.AppendLine(entity.ToString());
+            var fileFullName = string.Format(@"{0}\test.csv", Path);
+
+            File.WriteAllText(fileFullName, sb.ToString());
+ 
+
+        }
+
+        public static string ReadCommand()
+        {
+            string text = File.ReadAllText(@"E:\Test\cmd.txt");
+            return text;
+        }
+
+        public static string ReeadEntry()
+        {
+            string text = File.ReadAllText(@"E:\Test\UnivData.txt");
+            return text;
+        }
+
+
+
+    }
+}
